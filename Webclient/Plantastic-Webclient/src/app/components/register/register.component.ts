8a4899af2e8557d904ecfacba851997b0e615cd7
ClementsWagner <clements.wagner@gmx.at>
2021-12-15 16:20:27 +0100

    Added validation for signup

--- Webclient/Plantastic-Webclient/src/app/components/register/register.component.ts
+++ Webclient/Plantastic-Webclient/src/app/components/register/register.component.ts
@@ -5,6 +5,7 @@ import { NewUser } from 'src/app/models/new-user';
 import { User } from 'src/app/models/user';
 import { UserRestService } from 'src/app/services/Rest-services/user-rest.service';
 import { UserService } from 'src/app/services/user.service';
+import { CustomValidators } from 'src/app/providers/customValidator';
 
 @Component({
   selector: 'app-register',
@@ -16,10 +17,11 @@ export class RegisterComponent{
   newUser: NewUser = {email: "", password: ""}
   user: User = {userId: 0, email: ""}
 
-  passwordForm = new FormGroup({
-    password: new FormControl("password", Validators.required),
-    confirmPassword: new FormControl("confirmPassword", Validators.required)
-  })
+  signUpForm = new FormGroup({
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [Validators.required, Validators.minLength(8)]),
+    confirmPassword: new FormControl("", Validators.required),
+    });
 
   constructor(private userApi: UserRestService, private userService: UserService, private router: Router) {
 
@@ -39,6 +41,20 @@ export class RegisterComponent{
   createUser(){
     const result = this.userApi.addUser(this.newUser)
     result.subscribe(value => this.user = value)
+  }
+
+  checkIfMatchingPasswords(passwordKey: string, passwordConfirmationKey: string){
+    var password = this.getFormfield('passwordKey')?.value
+    var confirmPassword = this.getFormfield('passwordConfirmationKey')?.value
+    return password==confirmPassword
+  }
+
+  getFormfield(fieldName: string){
+    return this.signUpForm.get(fieldName)
+  }
+
+  isFieldValid(fieldName: string){
+    return !this.getFormfield(fieldName)?.valid && (this.getFormfield(fieldName)?.touched && this.getFormfield(fieldName)?.dirty)
   }
 
 }
---
+++ Webclient/Plantastic-Webclient/src/app/providers/customValidator.ts
@@ -0,0 +1,27 @@
+import {
+  AbstractControl,
+  ValidatorFn,
+  FormControl,
+  FormGroup,
+  ValidationErrors
+} from '@angular/forms';
+
+export class CustomValidators {
+  constructor() {}
+
+  static onlyChar(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: boolean } | null => {
+      if (control.value == '') return null;
+
+      let re = new RegExp('^[a-zA-Z ]*$');
+      if (re.test(control.value)) {
+        return null;
+      } else {
+        return { onlyChar: true };
+      }
+    };
+  }
+  static mustMatch(controlName: string, matchingControlName: string){
+
+  }
+}
