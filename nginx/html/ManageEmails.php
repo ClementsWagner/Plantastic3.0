29fd7a9dc0670624a47fff03d19f2c5e6b813937
ClementsWagner <clements.wagner@gmx.at>
2022-03-05 16:58:23 +0100

    Added email CRUD functions

---
+++ nginx/html/ManageEmails.php
@@ -0,0 +1,37 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['add']))
+{
+    addEmail($_POST['email']);
+}
+
+if($_SERVER['REQUEST_METHOD'] == "GET" and isset($_GET['remove']))
+{
+    echo 'removed';
+    removeEmail($_GET['email']);
+}
+
+
+function getEmails(){
+    $emailString = file_get_contents('emails.csv');
+    $emailList = explode(';', $emailString);
+    return $emailList;
+}
+
+function addEmail($email){
+    file_put_contents('emails.csv', $email.';', FILE_APPEND);
+}
+
+function removeEmail($del_email){
+    $emailList = getEmails();
+    if (($key = array_search($del_email, $emailList)) !== false) {
+        unset($emailList[$key]);
+    }
+    $emailString = "";
+    foreach($emailList as $email){
+        $emailString = $emailString . $email . ';';
+    }
+    file_put_contents('emails.csv', $emailString);
+}
+
+?>
